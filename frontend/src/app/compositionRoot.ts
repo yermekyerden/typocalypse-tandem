4b9519178680b1bc1cba3a57b74df8ac4890d184
Alessya Tkachenko <tkachenko.alessya@gmail.com>
2026-03-01 23:38:57 +0500

    feat(frontend): add new path for authentication to the compositionRoot

--- frontend/src/app/compositionRoot.ts
+++ frontend/src/app/compositionRoot.ts
@@ -1,6 +1,7 @@
 import { createHashRouter } from 'react-router-dom';
 
 import { AppShell } from '../ui/components/AppShell';
+import { AuthScreen } from '../ui/screens/authentication/AuthScreen';
 import { LibraryScreen } from '../ui/screens/library/LibraryScreen';
 import { MissionRunScreen } from '../ui/screens/mission-run/MissionRunScreen';
 import { ReplayScreen } from '../ui/screens/replay/ReplayScreen';
@@ -8,6 +9,10 @@ import { NotFoundScreen } from '../ui/screens/not-found/NotFoundScreen';
 
 export function createAppRouter() {
   return createHashRouter([
+    {
+      path: '/auth',
+      Component: AuthScreen,
+    },
     {
       path: '/',
       Component: AppShell,
--- frontend/src/authentication/auth.ts
+++
@@ -1,1 +0,0 @@
-//my code will be here
