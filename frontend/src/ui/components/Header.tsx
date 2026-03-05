67cd5842014cc1e30e6f220dec926fa66c2a1578
Roman Plis <plisroman@gmail.com>
2026-03-05 22:55:51 +0500

    feat(romanplis): add favicon

--- frontend/src/ui/components/Header.tsx
+++ frontend/src/ui/components/Header.tsx
@@ -7,11 +7,14 @@ export function Header() {
     <div>
       <header className="bg-gradient-to-b from-mist-950 to-mist-800 text-yellow-400 p-1">
         <div className="mx-auto flex items-center justify-between max-w-8xl px-4 py-1">
-          <Link to="/" className="flex items-center gap-2">
+          <Link
+            to="/"
+            className="flex items-center gap-2 transition hover:drop-shadow-[0_0_10px_rgba(250,204,21,0.9)]"
+          >
             <img src={Logo} alt="Terminal Dojo" className="h-8 w-8" />
             <span className="font-semibold text-yellow-400 text-lg">Terminal Dojo</span>
           </Link>
-          <Avatar></Avatar>
+          <Avatar onClick={() => console.log('Тестируем онклик!')}></Avatar>
         </div>
       </header>
     </div>
