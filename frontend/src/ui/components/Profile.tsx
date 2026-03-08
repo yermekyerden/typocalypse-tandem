08149ca12e8c09ec976f079eeb8ed93d61e35836
Denis Nasonov <brain0991@gmail.com>
2026-03-08 16:35:49 +0300

    feat(profile): add sidebar menu with navigation

--- frontend/src/ui/components/Profile.tsx
+++ frontend/src/ui/components/Profile.tsx
@@ -1,9 +1,11 @@
 import { cn } from '@/lib/utils';
+import { useState } from 'react';
 import { userData } from '@/mocks/user-data';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
-import { User } from 'lucide-react';
+
+type TabType = 'user-data' | 'progress' | 'settings';
 
 function getInitials() {
   const userName = userData.userName
@@ -36,62 +38,149 @@ export function UserAvatar() {
 }
 
 export function ProfilePage() {
+  const [activeTab, setActiveTab] = useState<TabType>('user-data');
+
   return (
     <div className="px-4 py-6 bg-linear-to-b from-mist-950 to-mist-800 flex-1">
-      <div className="mx-auto h-full">
-        <div className="rounded-2xl bg-[#2c2c2c] p-8 shadow-xl backdrop-blur-sm h-full flex flex-col md:justify-between">
-          <h1 className="mb-8 text-4xl font-bold text-yellow-400">{userData.userName}</h1>
-
-          <div className="flex mb-8">
-            <Avatar className="h-24 w-24 ring-4 ring-yellow-400/50">
-              <AvatarFallback className="bg-[#2c2c2c] text-yellow-400">
-                <User className="h-12 w-12 stroke-yellow-400" />
-              </AvatarFallback>
-            </Avatar>
-          </div>
-
-          <div className="space-y-6">
-            <div className="flex flex-col gap-2 rounded-lg bg-gray-700/50 p-4 md:flex-row md:items-center md:justify-between max-w-[50%]">
-              <span className="text-sm uppercase tracking-wider text-gray-400">
-                Username
-              </span>
-              <span className="text-lg text-yellow-400">{userData.userName}</span>
-            </div>
-
-            <div className="flex flex-col gap-2 rounded-lg bg-gray-700/50 p-4 md:flex-row md:items-center md:justify-between max-w-[50%]">
-              <span className="text-sm uppercase tracking-wider text-gray-400">
-                Login
-              </span>
-              <span className="text-lg text-yellow-400">{userData.login}</span>
-            </div>
-
-            <div className="flex flex-col gap-2 rounded-lg bg-gray-700/50 p-4 md:flex-row md:items-center md:justify-between max-w-[50%]">
-              <span className="text-sm uppercase tracking-wider text-gray-400">
-                Email
-              </span>
-              <span className="text-lg text-yellow-400">{userData.email}</span>
-            </div>
-
-            <div className="flex flex-col gap-2 rounded-lg bg-gray-700/50 p-4 md:flex-row md:items-center md:justify-between max-w-[50%]">
-              <span className="text-sm uppercase tracking-wider text-gray-400">
-                Password
-              </span>
-              <span className="text-lg text-yellow-400">********</span>
-            </div>
-          </div>
-
-          <div className="mt-8 flex justify-end">
-            <button
+      <div className="mx-auto h-full flex gap-3">
+        <div className="rounded-2xl bg-[#2c2c2c] h-full w-[25%] min-w-43.75 max-w-67.5 flex flex-col gap-2 px-2 py-2">
+          <div
+            onClick={() => setActiveTab('user-data')}
+            className={cn(
+              'flex flex-col gap-2 rounded-lg bg-[#3f4044] p-4',
+              'md:flex-row md:items-center md:justify-between w-full cursor-pointer group',
+            )}
+          >
+            <span
               className={cn(
-                'px-6 py-3 font-medium transition-all duration-200 rounded-[30px]',
-                'bg-[#ffd101] text-gray-900',
-                'hover:bg-yellow-300 hover:shadow-lg hover:shadow-yellow-400/20',
-                'active:scale-95 cursor-pointer',
+                'text-sm uppercase tracking-wider text-white',
+                activeTab === 'user-data'
+                  ? 'text-yellow-400'
+                  : 'text-white group-hover:text-yellow-400',
               )}
             >
-              Change data
-            </button>
+              User data
+            </span>
           </div>
+
+          <div
+            onClick={() => setActiveTab('progress')}
+            className={cn(
+              'flex flex-col gap-2 rounded-lg bg-[#3f4044] p-4',
+              'md:flex-row md:items-center md:justify-between w-full cursor-pointer group',
+            )}
+          >
+            <span
+              className={cn(
+                'text-sm uppercase tracking-wider text-white',
+                activeTab === 'progress'
+                  ? 'text-yellow-400'
+                  : 'text-white group-hover:text-yellow-400',
+              )}
+            >
+              Progress
+            </span>
+          </div>
+
+          <div
+            onClick={() => setActiveTab('settings')}
+            className={cn(
+              'flex flex-col gap-2 rounded-lg bg-[#3f4044] p-4',
+              'md:flex-row md:items-center md:justify-between w-full cursor-pointer group',
+            )}
+          >
+            <span
+              className={cn(
+                'text-sm uppercase tracking-wider text-white',
+                activeTab === 'settings'
+                  ? 'text-yellow-400'
+                  : 'text-white group-hover:text-yellow-400',
+              )}
+            >
+              Settings
+            </span>
+          </div>
+        </div>
+
+        <div className="rounded-2xl bg-[#2c2c2c] p-8 shadow-xl backdrop-blur-sm h-full w-full flex flex-col md:justify-between">
+          {activeTab === 'user-data' && (
+            <>
+              <h1 className="mb-8 text-4xl font-bold text-yellow-400">
+                {userData.userName}
+              </h1>
+
+              <div className="flex mb-8">
+                <Avatar className="h-24 w-24 ring-4 ring-yellow-400/50">
+                  <AvatarFallback className="bg-[#2c2c2c] text-yellow-400 text-4xl">
+                    {getInitials()}
+                  </AvatarFallback>
+                </Avatar>
+              </div>
+
+              <div className="space-y-6">
+                <div
+                  className={cn(
+                    'flex flex-col gap-2 rounded-lg bg-[#3f4044] p-4',
+                    'md:flex-row md:items-center md:justify-between max-w-[50%]',
+                  )}
+                >
+                  <span className="text-sm uppercase tracking-wider text-white">
+                    Username
+                  </span>
+                  <span className="text-lg text-yellow-400">{userData.userName}</span>
+                </div>
+
+                <div
+                  className={cn(
+                    'flex flex-col gap-2 rounded-lg bg-[#3f4044] p-4',
+                    'md:flex-row md:items-center md:justify-between max-w-[50%]',
+                  )}
+                >
+                  <span className="text-sm uppercase tracking-wider text-white">
+                    Login
+                  </span>
+                  <span className="text-lg text-yellow-400">{userData.login}</span>
+                </div>
+
+                <div
+                  className={cn(
+                    'flex flex-col gap-2 rounded-lg bg-[#3f4044] p-4',
+                    'md:flex-row md:items-center md:justify-between max-w-[50%]',
+                  )}
+                >
+                  <span className="text-sm uppercase tracking-wider text-white">
+                    Email
+                  </span>
+                  <span className="text-lg text-yellow-400">{userData.email}</span>
+                </div>
+
+                <div
+                  className={cn(
+                    'flex flex-col gap-2 rounded-lg bg-[#3f4044] p-4',
+                    'md:flex-row md:items-center md:justify-between max-w-[50%]',
+                  )}
+                >
+                  <span className="text-sm uppercase tracking-wider text-white">
+                    Password
+                  </span>
+                  <span className="text-lg text-yellow-400">********</span>
+                </div>
+              </div>
+
+              <div className="mt-8 flex justify-end">
+                <button
+                  className={cn(
+                    'px-6 py-3 font-medium transition-all duration-200 rounded-[30px]',
+                    'bg-[#ffd101] text-gray-900',
+                    'hover:bg-yellow-300 hover:shadow-lg hover:shadow-yellow-400/20',
+                    'active:scale-95 cursor-pointer',
+                  )}
+                >
+                  Change data
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
